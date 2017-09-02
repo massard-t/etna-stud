0091b4846163dffdb1e5cb6f713c1823ba9dc8f3
Theo Massard <massar_t@etna-alternance.net>
2017-09-02 18:08:09 +0200

    Add test route and index route Index routes servers index.html

--- app.py
+++ app.py
@@ -4,9 +4,25 @@
 Main app file for etna-stud
 """
 from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)  # pylint: disable=invalid-name
+
+
+@app.route('/')
+def index():
+    """Test route"""
+    return 'Test'
+
+
+@app.route('/index')
+def base_index():
+    """Index route"""
+    return render_template('index.html')
+
 
 def main():
-    app = Flask(__name__)
+    """Runs the server"""
 
     app.run(host='127.0.0.1', port=8080, debug=True)
 
